50eb0eb47a6c01eb00a5d5b42eecbb669263045a
Floris <florisdh@gmail.com>
2015-02-11 23:29:10 +0100

    Update 1
    - ProjectSettings
    - Thumb-pad
    - Horizontal movement with thumb-pad
    - Camera movement

---
+++ Assets/Scripts/Game/UID/CameraMovementScript.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovementScript : MonoBehaviour
+{
+    #region Properties
+
+    public float Force = 4f;
+    public float BreakSpeed = 3f;
+    public GameObject FollowingObject;
+
+    #endregion
+
+    #region Vars
+
+    private Vector3 _velo = Vector3.zero;
+
+    #endregion
+
+    #region Methods
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        // Horizontal distance
+        float dis = FollowingObject.transform.position.x - transform.position.x;
+
+        // Too far from target
+        if (Mathf.Abs(dis) >= 5f)
+        {
+            // Normalize * Scale by force
+            float step = dis / Mathf.Abs(dis);
+            step *= Force;
+
+            // Move to target
+            _velo = new Vector3(step, 0, 0);
+        }
+
+        // Apply velocity
+        if (_velo != Vector3.zero)
+        {
+            // Apply velocity
+            transform.position += _velo * Time.deltaTime;
+
+            // Decellerate
+            _velo -= _velo.normalized * BreakSpeed;
+
+            // Stop if too close
+            if (_velo.magnitude < BreakSpeed)
+            {
+                _velo *= 0;
+            }
+        }
+    }
+
+    #endregion
+}
---
+++ Assets/Scripts/Game/UID/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+    #region Properties
+
+    public ThumbStick InputStick;
+    public float Speed = 3f;
+
+    #endregion
+
+    #region Vars
+
+    private Vector3 _velo = Vector2.zero;
+
+    #endregion
+
+    #region Methods
+
+    void Update()
+    {
+        // Set velo
+        _velo.x = InputStick.StickUnitDirection.x * Speed;
+
+        // Apply velo
+        transform.position += _velo * Time.deltaTime;
+    }
+
+    #endregion
+
+}
